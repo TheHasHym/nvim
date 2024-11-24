51d7ecca1ceef563165f8f3909764b7b0193a378
Hasse Vestergaard Hymøller <hassevgaard@gmail.com>
2024-11-24 20:19:20 +0100

    Bug fix in remap

--- lua/hashym/remap.lua
+++ lua/hashym/remap.lua
@@ -9,7 +9,7 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Stay at start of line when moving lines toghter
-vim.keymap.set("n", "J", "mzJ`z"
+vim.keymap.set("n", "J", "mzJ`z")
 
 -- Stay centered when half page jumping
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
