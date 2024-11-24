8db562e9678fcbeeeb8668da5604eca914df6423
Hasse Vestergaard Hymøller <hassevgaard@gmail.com>
2024-11-24 20:17:14 +0100

    Add remaps & ansible-vim

--- lua/hashym/remap.lua
+++ lua/hashym/remap.lua
@@ -1,5 +1,29 @@
+-- Leader key
 vim.g.mapleader = ' '
+
+-- Open Netwr Explorer
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
+-- Move multiple lines with correct indentation
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "J", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Stay at start of line when moving lines toghter
+vim.keymap.set("n", "J", "mzJ`z"
+
+-- Stay centered when half page jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Stay centered when finding
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Copy to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Delete to void register
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
